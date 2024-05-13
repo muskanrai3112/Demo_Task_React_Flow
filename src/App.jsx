cad367bfe0e8154e4ada41241a7584e043a99346
Muskan Rai <muskanofficial3112@gamil.com>
2024-05-13 19:14:01 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import { useState } from "react";
 
 const App = () => {
   const showForm = useState(false);
- 
+
   return (
     <div>
-      <div style={{ height: "100vh", width: '100vw' }}>
-        <Flow showForm={showForm[1]}/>
+      <div style={{ height: "100vh", width: "100vw" }}>
+        <Flow showForm={showForm !== null ? showForm[1] : false} />
       </div>
       {showForm[0] && (
         <div style={{ position: "absolute", right: 50, top: 50 }}>
-          <MyForm showForm={showForm[1]} />
+          <MyForm showForm={showForm !== null ? showForm[1] : false} />
         </div>
       )}
     </div>
